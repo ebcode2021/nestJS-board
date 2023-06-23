be79345da34c8fbf96ba1c549e8bbb4d98890c14
ebcode2021 <ebcode2021@gmail.com>
2023-06-24 04:33:32 +0900

    [add] 회원가입

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,10 +1,18 @@
 import { CustomRepository } from "src/typeorm-ex/typeorm-ex.decorator";
 import { DataSource, Repository } from "typeorm";
+import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
 import { User } from "./user.entity";
 
 @CustomRepository(User)
 export class UserRepository extends Repository<User> {
 	constructor(private dataSource: DataSource) {
 		super(User, dataSource.createEntityManager());
+	}
+
+	async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+		const {username, password} = authCredentialsDto;
+		const user = this.create({ username, password});
+
+		await this.save(user);
 	}
 }
