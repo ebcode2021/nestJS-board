aff6bf6252b47d5351ba2d6ab9f14773456a2b5e
ebcode2021 <ebcode2021@gmail.com>
2023-06-24 02:21:30 +0900

    [modify] controller, service

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { BoardStatus } from './board-status.enum';
+import { Board } from './boards.entity';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
@@ -21,6 +22,10 @@ export class BoardsController {
 	// 	return this.boardsService.createBoard(createBoardDto);
 	// }
 
+	@Get('/:id')
+	getBoardById(@Param('id') id: number): Promise<Board> {
+		return this.boardsService.getBoardById(id);
+	}
 	// @Get('/:id')
 	// getBoardById(@Param('id') id: string): Board {
 	// 	return this.boardsService.getBoardById(id);
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -2,9 +2,16 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { BoardStatus } from './board-status.enum';
 import { v1 as uuid } from 'uuid';
 import { CreateBoardDto } from './dto/create-board.dto';
+import { BoardRepository } from './boards.repository';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Board } from './boards.entity';
 
 @Injectable()
 export class BoardsService {
+	constructor(
+		@InjectRepository(BoardRepository)
+		private boardRepository: BoardRepository,
+	){}
 
 	// getAllBoards(): Board[] {
 	// 	return this.boards;
@@ -23,6 +30,13 @@ export class BoardsService {
 	// 	return board;
 	// }
 
+	async getBoardById(id: number): Promise <Board> {
+		const found = await this.boardRepository.findOneBy({id});
+
+		if (!found)
+			throw new NotFoundException(`Can't find Board with id ${id}`);
+		return found;
+	}
 	// getBoardById(id: string) : Board {
 	// 	const found = this.boards.find((board) => board.id === id);
 		
