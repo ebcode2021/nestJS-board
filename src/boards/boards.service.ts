be57c805b05364882a6e6cbea6dda7e46aafce36
ebcode2021 <ebcode2021@gmail.com>
2023-06-24 02:07:22 +0900

    [modify] change service, controller because of do not use local memory storage

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,46 +1,44 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { Board, BoardStatus } from './boards.model';
+import { BoardStatus } from './board-status.enum';
 import { v1 as uuid } from 'uuid';
 import { CreateBoardDto } from './dto/create-board.dto';
 
 @Injectable()
 export class BoardsService {
-	private	boards: Board[] = [];
 
+	// getAllBoards(): Board[] {
+	// 	return this.boards;
+	// }
 
-	getAllBoards(): Board[] {
-		return this.boards;
-	}
+	// createBoard(createBoardDto: CreateBoardDto): Board {
+	// 	const {title, description} = createBoardDto;
+	// 	const board: Board = {
+	// 		id : uuid(),
+	// 		title,
+	// 		description,
+	// 		status: BoardStatus.PUBLIC
+	// 	}
 
-	createBoard(createBoardDto: CreateBoardDto): Board {
-		const {title, description} = createBoardDto;
-		const board: Board = {
-			id : uuid(),
-			title,
-			description,
-			status: BoardStatus.PUBLIC
-		}
+	// 	this.boards.push(board);
+	// 	return board;
+	// }
 
-		this.boards.push(board);
-		return board;
-	}
-
-	getBoardById(id: string) : Board {
-		const found = this.boards.find((board) => board.id === id);
+	// getBoardById(id: string) : Board {
+	// 	const found = this.boards.find((board) => board.id === id);
 		
-		if (!found)
-			throw new NotFoundException(`${id}란 아이디는 없습니다!`);
-		return found;
-	}
+	// 	if (!found)
+	// 		throw new NotFoundException(`${id}란 아이디는 없습니다!`);
+	// 	return found;
+	// }
 
-	deleteBoard(id: string) : void {
-		const found = this.getBoardById(id);
-		this.boards = this.boards.filter((board) => board.id !== found.id);
-	}
+	// deleteBoard(id: string) : void {
+	// 	const found = this.getBoardById(id);
+	// 	this.boards = this.boards.filter((board) => board.id !== found.id);
+	// }
 
-	updateBoardStatus(id: string, status: BoardStatus): Board {
-		const board = this.getBoardById(id);
-		board.status = status;
-		return board;
-	}
+	// updateBoardStatus(id: string, status: BoardStatus): Board {
+	// 	const board = this.getBoardById(id);
+	// 	board.status = status;
+	// 	return board;
+	// }
 }
